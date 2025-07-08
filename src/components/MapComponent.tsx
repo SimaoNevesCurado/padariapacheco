d55c6ad8657e931bca2d89c390eb09b95eba0069
simaonevescurado <simao.curadoo@gmail.com>
2025-07-08 13:08:17 +0100

    map fix

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -6,7 +6,7 @@ export const MapComponent = () => {
       </h2>
       <div className="aspect-w-16 aspect-h-9 bg-gray-200 rounded-lg overflow-hidden">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3113.073955759966!2d-9.142322684756612!3d38.71376077959938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzjCsDQyJzQ5LjUiTiA5wrAwOCcyNy4wIlc!5e0!3m2!1sen!2spt!4v1620000000000!5m2!1sen!2spt"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d252.83888426041008!2d-8.832056870281402!3d39.89731146000295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1spt-PT!2spt!4v1751976443011!5m2!1spt-PT!2spt"
           width="100%"
           height="450"
           style={{ border: 0 }}
